ca418d7c6129cdef67e353263a71d0c43e7a9710
Par Svanstrom <svanstrom@gmail.com>
2015-05-05 17:51:55 +0200

    added script for scheduled cleanup of old timelapse movies

---
+++ scripts/cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: `basename $0` <camera> [days old]"
+  exit
+fi
+
+WORKDIR=$(dirname $(dirname $0))
+CAMERA=$1
+
+if [ -z "$2" ] 
+then
+  DAYS_OLD=5
+else
+  DAYS_OLD=$2
+fi
+
+VIDEO_PATH=$WORKDIR/videos/$CAMERA/
+
+find $VIDEO_PATH -mtime +$DAYS_OLD -exec rm {} \;
